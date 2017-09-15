98c598e005fdcf1947349ae7b2ef4e81903d0996
Mehdi Abaakouk <sileht@sileht.net>
2017-09-15 19:44:26 +0200

    imap: fix memory leak
    In imap_commit_message_tags() cmd is not freed in case of error.

--- imap/imap.c
+++ imap/imap.c
@@ -1398,7 +1398,10 @@ static int imap_commit_message_tags(struct Context *ctx, struct Header *h, char 
      * continue to add new keywords *
      */
     if (imap_exec(idata, cmd->data, 0) != 0)
+    {
+      mutt_buffer_free(&cmd);
       return -1;
+    }
 
     mutt_buffer_free(&cmd);
   }
@@ -1421,6 +1424,7 @@ static int imap_commit_message_tags(struct Context *ctx, struct Header *h, char 
     if (imap_exec(idata, cmd->data, 0) != 0)
     {
       mutt_debug(1, "imap_commit_message_tags: fail to add new keywords\n");
+      mutt_buffer_free(&cmd);
       return -1;
     }
 
